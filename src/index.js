d98aa142ef66f8f110896261ba7ea5aeab59314a
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-10-25 23:56:55 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+let insults = [
+  "bocabuzón",
+  "bocabuzon",
+  "comechapas",
+  "mascachapas",
+  "gilipollas",
+  "subnormal",
+  "me cago en tus muertos",
+  "cabron de mierda",
+  "hijo de puta",
+  "retrasado mental",
+  "puerco de mierda",
+  "rata inmunda",
+  "rata imunda",
+  "lerdo",
+  "lameculos",
+  "perroflauta",
+  "maricon",
+  "sabandija",
+  "zopenco",
+  "hijueputa",
+];
+
+console.log("[insultsjs] -- ON");
+
+module.exports = { insults };
